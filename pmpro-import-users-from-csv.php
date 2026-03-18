66b07c771d9767e3ea597e6c1f7e5a5b23eede0c
Andrew Lima <andrew@arctek.co.za>
2026-03-18 16:00:24 +0200

    Fix issue for custom user meta
    * BUG FIX: Fixes an issue where custom user meta did not import correctly.

--- pmpro-import-users-from-csv.php
+++ pmpro-import-users-from-csv.php
@@ -1131,7 +1131,7 @@ class PMPro_Import_Users_From_CSV {
 											<?php endforeach; ?>
 										</optgroup>
 										<?php endforeach; ?>
-										<option value="_custom_"><?php esc_html_e( 'Custom User Meta', 'pmpro-import-users-from-csv' ); ?></option>
+										<option value="custom:<?php echo esc_attr( sanitize_key( $header ) ); ?>"><?php esc_html_e( 'Custom User Meta', 'pmpro-import-users-from-csv' ); ?></option>
 										
 									</select>
 									<p class="pmproiucsv-custom-hint" style="display:none; margin:4px 0 0; color:#646970; font-style:italic;"><?php printf( esc_html__( 'Will be saved as meta key: %s', 'pmpro-import-users-from-csv' ), '<code>' . esc_html( sanitize_key( $header ) ) . '</code>' ); ?></p>
@@ -1145,7 +1145,7 @@ class PMPro_Import_Users_From_CSV {
 					jQuery( document ).ready( function( $ ) {
 						$( '.pmproiucsv-field-select' ).on( 'change', function() {
 							var $hint = $( this ).closest( 'td' ).find( '.pmproiucsv-custom-hint' );
-							if ( $( this ).val() === '_custom_' ) {
+							if ( $( this ).val().indexOf( 'custom:' ) === 0 ) {
 								$hint.show();
 							} else {
 								$hint.hide();
